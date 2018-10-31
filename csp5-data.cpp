784e7b473863ef09e8fee98b899fa7fa9732604f
EAirPeter <VioletCrestfall@hotmail.com>
2018-10-31 19:58:03 +0900

    ~data: latest = 98v

--- csp5-data.cpp
+++ csp5-data.cpp
@@ -8,11 +8,8 @@ using namespace std;
 int main() {
     ofstream fo("t.txt");
     mt19937 rng{random_device{}()};
-    int n = 1000000;
-    fo << n << ' ' << n << endl;
-    for (int i = 0; i <= n; ++i)
-        fo << rng() % 10 << ' ';
-    fo << endl;
+    int n = 100000;
+    fo << n << " 1000000000000 1000000100000" << endl;
     for (int i = 0; i <= n; ++i)
         fo << rng() % 10 << ' ';
     fo << endl;
